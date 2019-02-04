72a9a62b2f643d75cb3d6a052330e1dc81e23e80
Timofey Kazantsev <pianoist@ya.ru>
2019-02-05 01:13:57 +0700

    autoformat

--- pyksp/compiler/base_types.py
+++ pyksp/compiler/base_types.py
@@ -37,8 +37,7 @@ class AstAssign(AstBase):
             if not self._to_arg:
                 raise TypeError('variable val is None')
         else:
-            raise TypeError(
-                f'can assign only to instance of {KspVar}')
+            raise TypeError(f'can assign only to instance of {KspVar}')
         if isinstance(from_arg, AstAssign):
             raise TypeError('AstAssign is root, can not be added')
         if callable(from_arg):
@@ -51,8 +50,7 @@ class AstAssign(AstBase):
             self._from_arg = from_arg.name()
         else:
             raise TypeError('can assign only instances of: %s' % (
-                (KspVar, str, int, float, AstBase)
-            ))
+                (KspVar, str, int, float, AstBase)))
 
     def expand(self):
         '''expand AstObject to string representation "a := b"'''
@@ -115,8 +113,7 @@ class AstAddString(AstBase):
         return AstAddString(other, self)
 
     def __iadd__(self, other):
-        raise NotImplementedError(
-            'method __iadd__ is not implemented')
+        raise NotImplementedError('method __iadd__ is not implemented')
 
 
 class AstOperator(AstBase):
@@ -140,9 +137,8 @@ class AstOperator(AstBase):
             if isinstance(arg, AstBase):
                 arg = arg.expand()
             if not isinstance(arg, (int, str, float)):
-                raise TypeError(
-                    f'arg {arg} has to be resolved to str,' +
-                    ' int or float')
+                raise TypeError(f'arg {arg} has to be resolved to str,' +
+                                ' int or float')
             new.append(arg)
         if len(new) == 1:
             new = new[0]
@@ -153,8 +149,7 @@ class AstOperator(AstBase):
         val = self.unpack_args(val)
         return f'{string}{val}'
 
-    def standart(self, string: str,
-                 val1: Union[int, str, float, KspObject],
+    def standart(self, string: str, val1: Union[int, str, float, KspObject],
                  val2: Union[int, str, float, KspObject]):
         '''returns f"{val1} {string} {val2}"'''
         pr = list()
@@ -212,8 +207,7 @@ class AstOperator(AstBase):
         return AstAdd(other, self)
 
     def __iadd__(self, other):
-        raise NotImplementedError(
-            'method __iadd__ is not implemented')
+        raise NotImplementedError('method __iadd__ is not implemented')
 
     def __sub__(self, other):
         return AstSub(self, other)
@@ -222,8 +216,7 @@ class AstOperator(AstBase):
         return AstSub(other, self)
 
     def __isub__(self, other):
-        raise NotImplementedError(
-            'method __isub__ is not implemented')
+        raise NotImplementedError('method __isub__ is not implemented')
 
     def __mul__(self, other):
         return AstMul(self, other)
@@ -232,8 +225,7 @@ class AstOperator(AstBase):
         return AstMul(other, self)
 
     def __imul__(self, other):
-        raise NotImplementedError(
-            'method __imul__ is not implemented')
+        raise NotImplementedError('method __imul__ is not implemented')
 
     def __truediv__(self, other):
         return AstDiv(self, other)
@@ -242,8 +234,7 @@ class AstOperator(AstBase):
         return AstDiv(other, self)
 
     def __itruediv__(self, other):
-        raise NotImplementedError(
-            'method __itruediv__ is not implemented')
+        raise NotImplementedError('method __itruediv__ is not implemented')
 
     def __floordiv__(self, other):
         raise ArithmeticError('use regular / instead')
@@ -252,8 +243,7 @@ class AstOperator(AstBase):
         raise ArithmeticError('use regular / instead')
 
     def __ifloordiv__(self, other):
-        raise NotImplementedError(
-            'method __ifloordiv__ is not implemented')
+        raise NotImplementedError('method __ifloordiv__ is not implemented')
 
     def __mod__(self, other):
         return AstMod(self, other)
@@ -262,8 +252,7 @@ class AstOperator(AstBase):
         return AstMod(other, self)
 
     def __imod__(self, other):
-        raise NotImplementedError(
-            'method __imod__ is not implemented')
+        raise NotImplementedError('method __imod__ is not implemented')
 
     def __pow__(self, other):
         return AstPow(self, other)
@@ -272,8 +261,7 @@ class AstOperator(AstBase):
         return AstPow(other, self)
 
     def __ipow__(self, other):
-        raise NotImplementedError(
-            'method __ipow__ is not implemented')
+        raise NotImplementedError('method __ipow__ is not implemented')
 
     def __and__(self, other):
         if self.is_bool():
@@ -416,7 +404,7 @@ class AstPow(AstOperator):
         return self.bracket_double('pow', val1, val2)
 
     def get_value(self):
-        return self.get_value_proxy(lambda arg1, arg2: arg1 ** arg2)
+        return self.get_value_proxy(lambda arg1, arg2: arg1**arg2)
 
 
 class AstLogAnd(AstOperator):
@@ -575,25 +563,30 @@ class KspVar(KspObject):
     KspVar will be used as ref_type.
     '''
 
-    def __init__(self, name, value=None,
+    def __init__(self,
+                 name,
+                 value=None,
                  ref_type=None,
                  name_prefix='',
                  name_postfix='',
                  preserve_name=False,
-                 has_init=True, is_local=False,
+                 has_init=True,
+                 is_local=False,
                  persist=False):
-        super().__init__(name, name_prefix=name_prefix,
-                         name_postfix=name_postfix,
-                         preserve_name=preserve_name,
-                         has_init=has_init, is_local=is_local,
-                         has_executable=False)
+        super().__init__(
+            name,
+            name_prefix=name_prefix,
+            name_postfix=name_postfix,
+            preserve_name=preserve_name,
+            has_init=has_init,
+            is_local=is_local,
+            has_executable=False)
         if ref_type:
             if not isinstance(ref_type, tuple):
                 raise TypeError('ref_type has to be tuple of classes')
             for item in ref_type:
                 if not isinstance(item, type):
-                    raise TypeError('ref_type has to be tuple' +
-                                    ' of classes')
+                    raise TypeError('ref_type has to be tuple' + ' of classes')
             self._ref_type = ref_type
         else:
             self._ref_type = self.__class__
@@ -627,8 +620,7 @@ class KspVar(KspObject):
         if self.is_local:
             raise RuntimeError('is local var')
         if self._read:
-            warn('read has been called yet', category=Warning,
-                 stacklevel=2)
+            warn('read has been called yet', category=Warning, stacklevel=2)
         Output().put(f'read_persistent_var({self.name()})')
         self._read = True
 
@@ -789,9 +781,8 @@ class KspNumeric(KspVar):
         '''raised when type convertion is needed'''
 
         def __init__(self, val):
-            super().__init__(
-                f'Value {val} (type{type(val)}) ' +
-                'has to be converted within built-in func')
+            super().__init__(f'Value {val} (type{type(val)}) ' +
+                             'has to be converted within built-in func')
 
     # def __new__(cls, *args, **kwargs):
     #     '''checks correct assignement of cls.warning_types'''
@@ -1023,7 +1014,6 @@ class KspNumeric(KspVar):
 
 
 class KspIntVar(KspNumeric):
-
     def __truediv__(self, other):
         self._warn_other(other)
         if self.is_compiled():
@@ -1109,20 +1099,20 @@ class KspRealVar(KspNumeric):
         if self.is_compiled():
             return AstPow(self, other)
         other = get_runtime(other)
-        return self._get_runtime() ** other
+        return self._get_runtime()**other
 
     def __rpow__(self, other):
         if self.is_compiled():
             return AstPow(other, self)
         other = get_runtime(other)
-        return self._get_runtime() ** other
+        return self._get_runtime()**other
 
     def __ipow__(self, other):
         if self.is_compiled():
             self._set_compiled(AstPow(self, other))
             return self
         other = get_runtime(other)
-        self._set_runtime(self._get_runtime() ** other)
+        self._set_runtime(self._get_runtime()**other)
         return self
 
     def __and__(self, other):
@@ -1155,22 +1145,32 @@ class KspArray(KspVar):
     kwarg value)
     '''
 
-    def __init__(self, name, name_prefix='', name_postfix='',
-                 preserve_name=False, has_init=True,
-                 is_local=False, ref_type=None, item_type=None,
-                 size=None, seq=None, persist=False, def_val=None):
-        super().__init__(name, name_prefix=name_prefix,
-                         ref_type=ref_type,
-                         name_postfix=name_postfix,
-                         preserve_name=preserve_name,
-                         has_init=has_init,
-                         is_local=is_local,
-                         persist=persist)
+    def __init__(self,
+                 name,
+                 name_prefix='',
+                 name_postfix='',
+                 preserve_name=False,
+                 has_init=True,
+                 is_local=False,
+                 ref_type=None,
+                 item_type=None,
+                 size=None,
+                 seq=None,
+                 persist=False,
+                 def_val=None):
+        super().__init__(
+            name,
+            name_prefix=name_prefix,
+            ref_type=ref_type,
+            name_postfix=name_postfix,
+            preserve_name=preserve_name,
+            has_init=has_init,
+            is_local=is_local,
+            persist=persist)
         if not isinstance(item_type, type):
             raise TypeError('item_type has to be subclass of KspVar')
             if not issubclass(item_type, KspVar):
-                raise TypeError(
-                    'item_type has to be subclass of KspVar')
+                raise TypeError('item_type has to be subclass of KspVar')
         self._item_type = item_type
 
         self.__pure_name = name
@@ -1183,8 +1183,7 @@ class KspArray(KspVar):
             if seq:
                 if len(seq) > size:
                     raise AttributeError(
-                        'size has to be greater or equal length of seq'
-                    )
+                        'size has to be greater or equal length of seq')
                 self._size = len(seq)
             else:
                 self._size = 0
@@ -1255,8 +1254,7 @@ class KspArray(KspVar):
     def append(self, val):
         '''puts value to the last used key of array, if size permits'''
         if not isinstance(val, self.ref_type):
-            raise TypeError(
-                f'has to be instance of {self.ref_type}')
+            raise TypeError(f'has to be instance of {self.ref_type}')
         if not self.in_init():
             raise RuntimeError('can not append outside init block')
         if not self._init_size:
@@ -1316,8 +1314,7 @@ class KspArray(KspVar):
         '''puts value to the cashed object at idx.
         Or makes one if not exists'''
         if not isinstance(val, self.ref_type):
-            raise TypeError(
-                f'has to be instance of {self.ref_type}')
+            raise TypeError(f'has to be instance of {self.ref_type}')
         runtime_idx = self._get_runtime_idx(idx)
         if self._size < runtime_idx:
             self._size = runtime_idx
@@ -1363,17 +1360,14 @@ class KspArray(KspVar):
             self._seq[idx] = self.__default
             val = self._seq[idx]
         if not isinstance(val, self.ref_type):
-            raise TypeError(
-                f'items has to be of type {self.ref_type}; ' +
-                f'returned {val} ' +
-                f'({type(val)})')
+            raise TypeError(f'items has to be of type {self.ref_type}; ' +
+                            f'returned {val} ' + f'({type(val)})')
         if isinstance(val, KspVar):
             val = val._get_runtime()
         if isinstance(val, AstBase):
             val = val.get_value()
-        item = self._item_type(name=f'{self.name()}[{idx}]',
-                               value=val,
-                               is_local=True)
+        item = self._item_type(
+            name=f'{self.name()}[{idx}]', value=val, is_local=True)
         self._cashed[idx] = item
         return True
 
@@ -1402,10 +1396,9 @@ class KspArray(KspVar):
         raise RuntimeError("can't assign to created array")
 
     def __iter__(self):
-        raise NotImplementedError(
-            'can not be used in iter.' +
-            'use self.iter_runtime or' +
-            'self.iter_runtime_fast')
+        raise NotImplementedError('can not be used in iter.' +
+                                  'use self.iter_runtime or' +
+                                  'self.iter_runtime_fast')
 
     def __len__(self):
         '''returns init size, if specifyed, or actual size of array.
