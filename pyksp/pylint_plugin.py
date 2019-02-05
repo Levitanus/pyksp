fc0ca7bd06cd6c8d8aed87da529bd4dc34fd5ffe
Timofey Kazantsev <pianoist@ya.ru>
2019-02-05 15:50:24 +0700

    pylint plugin

---
+++ pyksp/pylint_plugin.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2016, 2018 Claudiu Popa <pcmanticore@gmail.com>
+# Copyright (c) 2018 Bryce Guinta <bryce.paul.guinta@gmail.com>
+"""Astroid hooks for understanding functools library module."""
+from functools import partial
+
+import astroid  # type: ignore
+from astroid import MANAGER
+
+LRU_CACHE = "functools.lru_cache"
+
+
+def register(linter):  # type: ignore
+    pass
+
+
+def _vrs_inference(node, context=None):  # type: ignore
+    for idx, t in enumerate(
+            zip(node.args.kwonlyargs, node.args.kwonlyargs_annotations)):
+        arg, anno = t
+        attr = anno.value
+        if attr.attrname != 'Loc':
+            return (node, )
+        module = attr.expr.inferred()[0]
+        if module.name != 'pyksp.service_types':
+            return (node, )
+        del node.args.kwonlyargs[idx]
+        del node.args.kwonlyargs_annotations[idx]
+    return (node, )
+
+
+# MANAGER.register_transform(astroid.FunctionDef, _transform_lru_cache,
+#                            _looks_like_lru_cache)
+
+
+def _looks_like_service_types_member(node, member):  # type: ignore
+    """Check if the given Call node is a functools.partial call"""
+    if not node.decorators:
+        return
+    if not isinstance(node.decorators.nodes[0], astroid.Attribute):
+        return
+    attr = node.decorators.nodes[0]
+    if attr.attrname == 'vrs':
+        module = attr.expr.inferred()[0]
+        return module.name == 'pyksp.service_types'
+    return False
+
+
+_looks_like_partial = partial(_looks_like_service_types_member, member="vrs")
+
+MANAGER.register_transform(astroid.FunctionDef,
+                           astroid.inference_tip(_vrs_inference),
+                           _looks_like_partial)
--- pyksp/simple_test.py
+++ pyksp/simple_test.py
@@ -1,60 +1,19 @@
-import typing as ty
-import functools as ft
-import inspect as it
-import re
-if __name__ == '__main__':
-    __name__ = 'pyksp.simple_test'
-# from typing_extensions import Literal
-from pyksp import abstract as ab
-# from pyksp import base_types as bt
 from . import service_types as st
 
-T = ty.TypeVar('T')
-F = ty.TypeVar('F', bound=ty.Callable[..., None])
-IT = ty.TypeVar('IT', bound=int)
 
-
-def vrs(f: F) -> F:
-    name = f.__qualname__
-    name = re.sub(r'\.', '_', name)
-    ab.NameVar.scope(name)
-    sig = it.signature(f)
-    new_kwargs: ty.Dict[str, st.Loc] = dict()
-    for par in sig.parameters:
-        anno = sig.parameters[par].annotation
-        # print(par, anno)
-        if issubclass(anno, st.Loc):
-            # print('instance')
-            a_kwgs = dict(name=par)
-            if hasattr(anno, 'size'):
-                a_kwgs['size'] = anno.size
-            try:
-                new_kwargs[par] = anno(**a_kwgs)
-            except NameError:
-                new_kwargs[par] = anno(**a_kwgs, local=True)
-    ab.NameVar.scope()
-
-    @ft.wraps(f)
-    def wrapper(*args: ty.Any, **kwargs: ty.Any) -> None:
-        b_args = sig.bind(*args, **kwargs, **new_kwargs)
-        return f(*b_args.args, **b_args.kwargs)
-
-    return ty.cast(F, wrapper)
-
-
-@vrs
-def func(arg: int, *, arg1: st.Loc[str, 5]) -> None:
+@st.vrs
+def st_func(arg: int, *, arg1: st.Loc[str, 5]) -> None:
     print(arg, f'{arg1.name()}:{arg1.val}')
     # arg1[3] <<= 'f'  # type: ignore
 
 
 class C:
-    @vrs
-    def method(self, arg: int, *, arg1: st.Loc[str, 5]) -> None:
+    @st.vrs
+    def st_method(self, arg: int, *, arg1: st.Loc[str, 5]) -> None:
         print(arg, f'{arg1.name()}:{arg1.val}')
 
 
-func(1)  # pylint: disable=E1125
+st_func(1)
 c = C()
-c.method(3)  # pylint: disable=E1125
-c.method(5)  # pylint: disable=E1125
+c.st_method(3)
+c.st_method(5)
